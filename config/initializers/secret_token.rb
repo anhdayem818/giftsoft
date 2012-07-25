77f2adcb882a2b6caadc4b9f56267e744b3338b2
Will <phuongquang1212@gmail.com>
2012-07-25 16:41:49 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giftshop::Application.config.secret_token = '56acfd379a2d693e8c62fab822cf5be344670d3024df5061a45259dc16640b8fb3933b86d7b8bbe421929fab83adc6a54ca4a7dc10f52c634fa51df61b8c3212'
