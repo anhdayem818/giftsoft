0de71c897d0ccb5606c78fcdd5080470f94958fa
vinrex <phuongquang1212@gmail.com>
2012-08-20 21:40:47 +0700

    list all product in home page

---
+++ app/controllers/spree/home_controller_decorator.rb
@@ -0,0 +1,7 @@
+Spree::HomeController.class_eval do
+#  def index
+#    @latest_products = Spree::Product.limit(6).order(:created_at)
+#    @feature_products = Spree::Product.limit(6)
+#    respond_with(@latest_products)
+#  end
+end
