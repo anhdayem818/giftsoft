f1e8cc986b980b42cd7b8ad2fa0503c1bbd20f58
vinrex <phuongquang1212@gmail.com>
2012-08-20 13:39:13 +0700

    Fix order updating

--- app/assets/javascripts/admin/all.js
+++ app/assets/javascripts/admin/all.js
@@ -7,7 +7,4 @@
 //= require jquery
 //= require jquery_ujs
 
-//= require admin/spree_core
-//= require admin/spree_promo
-
 //= require_tree .
--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,5 +1,3 @@
 Spree::Order.class_eval do
-  def use_billing?
-    true
-  end
+  
 end
