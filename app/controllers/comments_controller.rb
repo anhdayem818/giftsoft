2e0667e1c33ec4e53e3dcc86aff6e628db19085b
vinrex <phuongquang1212@gmail.com>
2012-08-25 23:20:29 +0700

    Add gia_si link

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ActionController::Base
   def create
-    @product = Spree::Product.find(params[:product_id])
+    @product = Spree::Product.active.find_by_permalink!(params[:product_id])
     #params[:comment][:user_id] = current_user.id if current_user.present?
     comment = @product.comments.build(params[:comment])
     comment.user = current_user
