11c1643222f13486d545f9718e72a83bb652486f
vinrex <phuongquang1212@gmail.com>
2012-08-26 00:58:10 +0700

    validate unique username

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,5 +1,5 @@
 # Default implementation of User.  This class is intended to be modified by extensions (ex. spree_auth_devise)
 Spree::User.class_eval do
   attr_accessible :username
-  validates :username, :presence => true
+  validates :username, :presence => true, :uniqueness => true
 end
