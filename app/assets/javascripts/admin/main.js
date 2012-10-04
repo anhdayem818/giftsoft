21b05b16871d33071dd128960f8ad1fd8f466836
vinrex <phuongquang1212@gmail.com>
2012-10-04 14:12:00 +0700

    Show user's info in orders list

---
+++ app/assets/javascripts/admin/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    //$('[rel=tooltip]').tooltip({title: "test me", placement: 'bottom', trigger: 'manual'});
+    $('[rel=tooltip]').tipsy({html: true, gravity: 's', fade: true});
+});
