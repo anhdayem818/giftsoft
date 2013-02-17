9ba0ff6506c99de115b51179e52f7536d722890d
will <phuongquang1212@gmail.com>
2013-02-17 10:05:35 +0700

    Error message fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def title
     
   end
+  
+  def min_total
+    returning_customer? ? 200000 : 500000
+  end
+  def checkoutable?
+    item_total = current_order.present? ? current_order.item_total : 0
+    item_total >= min_total
+  end
+  
+  def returning_customer?
+    current_user.present? && current_user.orders.select(&:paid?).present?
+  end
 end
