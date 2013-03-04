1114967c47909bb12e6df90ffee810a1c2fb8c46
Will <nhpquang@deekau.com>
2013-03-04 14:57:17 +0700

    Fix log out issue when add item to order

--- app/assets/javascripts/admin/main.js
+++ app/assets/javascripts/admin/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
     //$('[rel=tooltip]').tooltip({title: "test me", placement: 'bottom', trigger: 'manual'});
     $('[rel=tooltip]').tipsy({html: true, gravity: 's', fade: true});
+    $.ajaxSetup({
+      beforeSend: function(xhr) {
+          xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
+      }
+    });
 });
