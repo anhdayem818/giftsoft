87143cbc580223cf0c68e68d4a4da3f344d18818
will <phuongquang1212@gmail.com>
2013-03-05 23:22:51 +0700

    Temp fix for taxon

---
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -0,0 +1,21 @@
+module Spree
+  module Admin
+    ProductsController.class_eval do
+      
+        def update_before
+          # note: we only reset the product properties if we're receiving a post from the form on that tab
+          #@product = @object
+          if params[:product].present?
+            @taxon = Taxon.find(params[:taxon_id])
+            if @taxon
+              @product.taxons << @taxon
+              @product.save
+              @taxons = @product.taxons
+            end
+          end
+          return unless params[:clear_product_properties]
+          params[:product] ||= {}
+        end
+    end
+  end
+end
