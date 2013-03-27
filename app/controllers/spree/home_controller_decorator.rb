be3936715e1084ca2722ac321f35015c80aae15d
will <phuongquang1212@gmail.com>
2013-03-27 23:43:57 +0700

    Show recent comments

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -4,6 +4,7 @@ Spree::HomeController.class_eval do
     @searcher = Spree::Config.searcher_class.new(params)
     @products = @searcher.retrieve_products.order("updated_at DESC")
     @announcements = Announcement.limit(5).order("created_at DESC")
+    @comments = Comment.limit(5).order("created_at DESC")
     respond_with(@products)
   end
   def set_current_page
