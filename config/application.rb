000928c1157c858e37a1a678a7f5857925bd1e5c
muamely <admin@muamely>
2013-04-08 13:44:49 +0400

    Fix decorator loading issue

--- config/application.rb
+++ config/application.rb
@@ -14,10 +14,13 @@ module Giftshop
     
     config.to_prepare do
       # Load application's model / class decorators
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+      #Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+      #  Rails.configuration.cache_classes ? require(c) : load(c)
+      #end
+
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
-
       # Load application's view overrides
       Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
