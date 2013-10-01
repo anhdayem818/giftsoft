e18f24904f549f85d0a7fb42c3f5990150dccef5
cpvinh <cpvinh@webmely.com>
2013-10-01 17:04:14 +0700

    Fix notify

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,7 +1,8 @@
 module NotificationsHelper
 
   def html_notify(notify)
-    html = "<div class=\"user-comment\"><span class=\"username\">" + notify.user_comment.username +
+    user_comment = notify.user_comment.present? ? notify.user_comment.username : (t "customer")
+    html = "<div class=\"user-comment\"><span class=\"username\">" + user_comment +
             "</span> " + (t "create_comment").downcase +  " SP</div><div class=\"notificationable-name\">" + notify.notificationable.name + "</div>"
     html.html_safe
   end
