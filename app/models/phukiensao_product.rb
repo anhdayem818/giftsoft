7a6ed6cd530cf6b8890752bb3b28221d9b8dae51
tbdieu <tbdieu@deekau.com>
2013-10-30 16:16:11 +0700

    Fix img clone for clone_product

--- app/models/phukiensao_product.rb
+++ app/models/phukiensao_product.rb
@@ -24,7 +24,7 @@ class PhukiensaoProduct < ActiveRecord::Base
         # set prepare to update url img
         desc = product.description
         puts "clone product save success"
-        product.variants_including_master.each do |v|
+        product.variants_including_master_and_deleted.each do |v|
           puts "variant of product: #{v.to_json} ---------------------------------"
           params = v.attributes
           params.delete("product_id")
@@ -39,29 +39,29 @@ class PhukiensaoProduct < ActiveRecord::Base
             puts "clone variant success"
             v.images.each do |img|
               puts "image of variant: #{img.to_json} -------------------------------"
-              params = img.attributes
-              max_id = PhukiensaoProductImage.maximum(:id) || 0
-              clone_image_id = max_id + 1
-              params.delete(:id)
-              clone_image = PhukiensaoProductImage.new(params)
-              clone_image.id = clone_image_id
-              clone_image.viewable_id = clone_variant.id
-              clone_image.attachment = URI.parse("http://muamely.com/spree/products/#{clone_image_id}/#{URI.encode(params['attachment_file_name'])}")
-              clone_image.attachment_file_name = params['attachment_file_name']
-              clone_image.attachment_content_type = params["attachment_content_type"]
-              puts "clone image"
-              clone_image.save
+              clone_image = PhukiensaoProductImage.clone_db(img, clone_variant.id)
 
-              desc = desc.gsub("spree/products/#{img.id}", "spree/products/#{clone_image_id}") if desc.include?("spree/products/#{img.id}")
+              desc = desc.gsub("spree/products/#{img.id}", "spree/products/#{clone_image.id}") if desc.include?("spree/products/#{img.id}")
 
-              #system "mkdir /home/wf04/Desktop/research-project/giftshop_clone/public/spree/products/#{clone_image_id}"
-              #system "cp -r /home/wf04/Desktop/research-project/giftshop/public/spree/products/#{img.id}/* /home/wf04/Desktop/research-project/giftshop_clone/public/spree/products/#{clone_image_id}"
-              system "mkdir /home/rails/phukiensao.com/public/spree/products/#{clone_image_id}"
-              system "cp -r /home/rails/muamely.com/public/spree/products/#{img.id}/* /home/rails/phukiensao.com/public/spree/products/#{clone_image_id}"
+            end
+          end
+        end
 
-              # update url img in description
-              #desc = desc.gsub("spree/products/#{img.id}", "spree/products/#{clone_image_id}") if desc.include?("spree/products/#{img.id}")
-
+        # add new image if not exist in variant
+        nokogiri_desc = Nokogiri::HTML(desc)
+        nokogiri_desc.search('img').each do |img_tag|
+          puts "tagggggggggggggggggggggggggggggggggggggggggggggggggg"
+          puts img_tag.attr('src')
+          puts img_tag.attr('src').include?("spree/products/")
+          puts !img_tag.attr('src').include?("http")
+          if img_tag.attr('src').include?("spree/products/") && !img_tag.attr('src').include?("http")
+            img_id = img_tag.attr('src').split('/products/')[1].split('/')[0]
+            puts "image iddddddddddddddddddd #{img_id}"
+            #img_id = img_tag.attr('src').gsub("../../../spree/products/", "").split("/")[0]
+            pks_img = PhukiensaoProductImage.find_by_id(img_id)
+            if pks_img.blank? || (pks_img.present? && pks_img.viewable.present? && pks_img.viewable.product.present? && pks_img.viewable.product.id != clone_product.id)
+              clone_image = PhukiensaoProductImage.clone_db(Spree::Image.find_by_id(img_id), nil) if Spree::Image.find_by_id(img_id).present?
+              desc = desc.gsub("spree/products/#{img_id}", "spree/products/#{clone_image.id}")
             end
           end
         end
