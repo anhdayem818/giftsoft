61628d11041991bbcd22dff32cc24768efa75e1d
tbdieu <tbdieu@deekau.com>
2013-10-30 23:01:02 +0700

    Adđ update price follow product, increase price clone product

--- app/models/phukiensao_product.rb
+++ app/models/phukiensao_product.rb
@@ -13,6 +13,7 @@ class PhukiensaoProduct < ActiveRecord::Base
       params.delete("is_clone")
       clone_product = self.new(params)
       clone_product.id = nil
+
       puts "clone product: #{clone_product.to_json}"
 
       if clone_product.save
@@ -24,6 +25,7 @@ class PhukiensaoProduct < ActiveRecord::Base
         # set prepare to update url img
         desc = product.description
         puts "clone product save success"
+
         product.variants_including_master_and_deleted.each do |v|
           puts "variant of product: #{v.to_json} ---------------------------------"
           params = v.attributes
@@ -31,6 +33,15 @@ class PhukiensaoProduct < ActiveRecord::Base
           params.delete("id")
           clone_variant = PhukiensaoVariant.new(params)
           clone_variant.product_id = clone_product.id
+
+          # optional , increase price
+          if clone_variant.price > 50000
+            clone_variant.price = clone_variant.price + 50000
+          else
+            clone_variant.price = clone_variant.price * 2
+          end
+
+
           puts "clone variant: #{clone_variant.to_json}"
           if clone_variant.save
             # clone option value
--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,5 +1,7 @@
 Spree::Variant.class_eval do
   attr_accessible :original_price
+  after_update :update_price_all_variants
+
   def in_process
     line_items.joins(:order).where("spree_orders.state = 'complete' AND spree_orders.payment_state <> 'paid'").sum(:quantity)
   end
@@ -7,5 +9,15 @@ Spree::Variant.class_eval do
   def sole_out(start_date = Time.now, end_date = Time.now)
     line_items.joins(:order).where(:spree_orders => {:state => 'complete', :payment_state => 'paid', :paid_at => start_date..end_date})
     .sum(:quantity)
+  end
+
+
+  private
+  def update_price_all_variants
+    if self.is_master? && product.present? && product.variants.present?
+      product = self.product
+      product.variants.update_all(price: self.price)
+    end
+
   end
 end
