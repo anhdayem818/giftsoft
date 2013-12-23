4b76927a201bb1c10d246ca45cd3fc769a619acc
nhpquang <nhpquang@deekau.com>
2013-12-23 16:23:29 +0700

    support barcode + new buyer ighlight

--- app/controllers/admin/barcodes_controller.rb
+++ app/controllers/admin/barcodes_controller.rb
@@ -3,8 +3,10 @@ module Admin
     #require 'barby/barcode/data_matrix'
     require 'barby/barcode/code_39'
     require 'barby/outputter/html_outputter'
+    
     #require 'barby/outputter/cairo_outputter'
     def show
+      render layout: false
     end
   end
 end
