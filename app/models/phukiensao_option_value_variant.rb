08b0b8c4c7d5e477e7b945074c6802ab284b15a5
will <phuongquang1212@gmail.com>
2015-01-16 20:18:07 +0700

    remove phukiensao stuffs

--- app/models/phukiensao_option_value_variant.rb
+++
@@ -1,9 +0,0 @@
-class PhukiensaoOptionValueVariant < ActiveRecord::Base
-  self.table_name = "spree_option_values_variants"
-  self.primary_key = "id"
-  attr_protected :id
-  attr_accessible :id, :variant_id, :option_value_id
-
-  establish_connection(:phukiensao_production)
-
-end
